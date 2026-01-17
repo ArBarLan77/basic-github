04283b4455d89482cf007433e9b89d0a017b562a
ArBarLan77 <arbarlan14@gmail.com>
2026-01-17 23:30:32 +0630

    Add files via upload

---
+++ arrayFE01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int arr[5]={10,20,33,24,28};
+/*	cout<<arr[0]<<endl;
+	cout<<arr[1]<<endl;
+	cout<<arr[2]<<endl;
+	cout<<arr[3]<<endl;
+	cout<<arr[4]<<endl;*/
+	
+	for(int i=0;i<5;i++){
+		//	cout<<i<<endl;	
+	//	cout<<arr[0]<<endl;	
+	cout<<arr[i]<<endl;
+		
+	}
+	
+	
+	
+	
+	return 0;
+	
+}
